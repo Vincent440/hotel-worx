f0a4d05ea0d99d75b20f9580b1d19f6a8d3ca61f
Sibel <sbaslamisli@yahoo.com>
2019-06-19 15:19:41 -0400

    dashboard file is created

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,8 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from './pages/home';
-import Header from './components/header';
-import Footer from './components/footer';
 import Login from "./components/login/login";
 import Particles from 'react-particles-js';
-
 
 const particleOpt = {
     particles: {
