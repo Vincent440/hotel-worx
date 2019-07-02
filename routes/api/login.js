dcf8167482f716b52c3e4e43638e6e0873e53017
Vince216 <vinceshury@gmail.com>
2019-07-02 10:47:03 -0400

    passport npm and dependences installed. on login

--- routes/api/login.js
+++ routes/api/login.js
@@ -1,30 +1,27 @@
 const router = require("express").Router();
 const passport = require("passport");
 require("../../controllers/passportController")(passport);
-
 // const db = require("../../models");
 
-// // route middleware to make sure
-// function isLoggedIn(req, res, next) {
-
-// 	// if user is authenticated in the session, carry on
-// 	if (req.isAuthenticated())
-// 		return next();
-
-// 	// if they aren't redirect them to the home page
-// 	res.redirect(req.session.returnTo || '/');
-// }    console.log(req.body);
-
-
-// passport.initialize("local"),(req, res) => {
-
-
-// }
 // '/api/login' route
-router.route("/")
-// POST route for submitting login data
-.post((req,res)=>{
-    res.status(200).json({user: true,body: req.body})
+
+router.route("/").post(// Using local strategy to redirect back to the signin page if there is an error
+    passport.authenticate('local', { failureRedirect : '/login' }),
+    function(req, res) {
+        debugger;
+        console.log(req.isAuthenticated());
+        console.log("line 11 routes/api/login.js route callback function | Request: "+ req);
+        // if (req.body.remember) {
+        //   req.session.cookie.maxAge = 1000 * 60 * 3;
+        // } else {
+        //  req.session.cookie.expires = false;
+        // }
+        res.status(200).json({ user: req.user } );
 });
+
+// POST route for submitting login data
+// .post((req,res)=>{
+//     res.status(200).json({user: true,body: req.body})
+// });
 
 module.exports = router;
