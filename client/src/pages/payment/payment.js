ab10a9a4e39d15a51a0aa25c43fdc914b64fb7b7
Sibel <sbaslamisli@yahoo.com>
2019-07-08 18:55:18 -0400

    page work

--- client/src/pages/payment/payment.js
+++ client/src/pages/payment/payment.js
@@ -4,6 +4,7 @@ import "./style3.css";
 import api from '../../utils/api';
 import InfoPart from "../../components/infoPart";
 import Header from "../../components/Header"
+import { Container, Table } from 'react-bootstrap';
 
 
 class Payment extends Component {
@@ -30,11 +31,12 @@ class Payment extends Component {
         ReservationInfo: {},
         RoomInfo: []
     };
-    componentDidMount() {
-        api.getReservation(1)
-            .then(res => this.setState({ ReservationInfo: res.resCust.result[0], RoomInfo: res.resRooms.result }))
-            .catch(err => console.log(err))
-    }
+
+    // componentdidMount() {
+    //     api.getreservation()
+    //         .then(res => this.setState({ ReservationInfo: res.resCust.result[0], RoomInfo: res.resRooms.result }))
+    //         .catch(err => console.log(err))
+    // }
 
     handleChange = selectedOption => {
         this.setState({ selectedOption });
@@ -54,7 +56,7 @@ class Payment extends Component {
 
     // When the form is submitted, prevent the default event and alert the username and password
     handleFormSubmit = event => {
-        event.preventDefault();
+        event.preventdefault();
         alert(`Username: ${this.state.username}\nPassword: ${this.state.password}`);
         this.setState({ username: "", password: "" });
     }
@@ -62,32 +64,39 @@ class Payment extends Component {
 
 
         return (
+            <Container>
+                <Row>
+                    <Col sm={2}>
+                        <InfoPart />
+                    </Col>
 
-            <Row id="dashboardTable1">
-                <InfoPart />
-                <Col sm={10}>
-                    <row>
-                        <form>
+                    <Col sm={10}>
+                        <Row>
+                            <Col xl={12}>
+                                <Header>BILLING</Header>
+                            </Col>
+                        </Row>
+                        <div id="res">
+
                             {this.state.RoomInfo.map((room, i) => (
-                                <div>
-                                    <Header>BILLING - Room: {room.room_num}</Header>
 
-                                    <div id="resInvoice">
-                                        <table border="1">
-                                            <tr>
-                                                <td>Balance: {this.state.balance}</td>
-                                                <td>Arrival Date: {room.check_in_date}</td>
-                                                <td>Daily Room Rate:{this.state.roomRate}</td>
-                                            </tr>
-                                            <tr>
-                                                <td>Status:</td>
-                                                <td>Departure Date: {room.check_out_date}</td>
-                                                <td>Room Type: {room.type}</td>
-                                            </tr>
+                                <div id="res" style={{ paddingBottom: "10px" }}>
 
-                                        </table>
-                                        <div>
-                                            <thead>
+                                    <Row>
+                                        <h2>Room: {room.room_num}</h2>
+                                        <Table border="1">
+                                            <tbody>
+                                                <tr>
+                                                    <td>Balance: {this.state.balance}</td>
+                                                    <td>Arrival Date: {room.check_in_date}</td>
+                                                    <td>Daily Room Rate:{this.state.roomRate}</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Status:</td>
+                                                    <td>Departure Date: {room.check_out_date}</td>
+                                                    <td>Room Type: {room.type}</td>
+                                                </tr>
+
                                                 <tr>
                                                     <th className="tableInfo">{this.state.ReservationInfo.last_name}, {this.state.ReservationInfo.first_name}</th>
                                                     <th className="tableInfo">CC: <input type="checkbox" id="myCheck" onmouseover="myFunction()" onclick="alert('click event occured')" />
@@ -95,35 +104,29 @@ class Payment extends Component {
                                                     <th className="tableInfo">Cash:   <input type="checkbox" id="myCheck" onmouseover="myFunction()" onclick="alert('click event occured')" />
                                                     </th>
                                                 </tr>
-                                            </thead>
-                                        </div>
-                                        <table id="result">
-                                            <thead>
                                                 <tr>
                                                     <th className="th" id="date">Date</th>
                                                     <th className="th" id="description">Description</th>
                                                     <th className="th" id="amount">Amount</th>
                                                 </tr>
-                                            </thead>
-                                        </table>
-                                    </div>
+                                            </tbody>
 
+                                        </Table>
+                                    </Row>
 
-                                    <div id="buttonDiv">
-                                        <button type="button" class="btn btn-primary" style={{marginLeft:"350px", marginTop:"10px"}}>Post</button>
-                                        <button type="button" class="btn btn-danger" style={{marginTop:"10px"}}>Payment</button>
-                                        <button type="button" class="btn btn-danger" style={{marginTop:"10px"}}>Check Out</button>
-                                        <button type="button" class="btn btn-primary" style={{marginTop:"10px", marginLeft:"5px"}}>Close</button>
-
-                                    </div>
+                                    <button type="button" class="btn btn-primary" style={{ marginLeft: "350px", marginTop: "10px" }}>Post</button>
+                                    <button type="button" class="btn btn-danger" style={{ marginTop: "10px" }}>Payment</button>
+                                    <button type="button" class="btn btn-danger" style={{ marginTop: "10px" }}>Check Out</button>
+                                    <button type="button" class="btn btn-primary" style={{ marginTop: "10px", marginLeft: "5px" }}>Close</button>
                                 </div>
-
                             ))}
+                        </div>
+                    </Col>
 
-                        </form>
-                    </row>
-                </Col >
-            </Row >
+
+                </Row >
+            </Container>
+
         )
     }
 }
