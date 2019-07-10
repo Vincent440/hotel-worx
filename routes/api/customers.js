9d747ca965bccfd4b6a70a7771c873fe2332abb3
Vince216 <vinceshury@gmail.com>
2019-07-10 13:20:47 -0400

    removed unneeded comments and logs

--- routes/api/customers.js
+++ routes/api/customers.js
@@ -6,7 +6,6 @@ router.route("/")
 // POST "/api/customers" Example Request:
 // { "vals": ["Joe", "Blow", "222 E Market St", "Akron", "Ohio", "42116", "joeblow@gmail.com", "440-234-1234", "n/a", 1] }
 .post(customerController.createNewCustomer)
-
 // GET "/api/customers"
 .get(customerController.getAllCustomers);
 
@@ -15,14 +14,11 @@ router.route("/")
 router.route("/:id")
 // GET "/api/customers/:id"
 .get(customerController.getCustomerById)
-
 // PUT "/api/customers/:id" Example Request:
 // { "vals": ["Joe", "Blow", "222 E Market St", "Akron", "Ohio", "42116", "joeblow@gmail.com", "440-234-1234", "n/a", 1] }
 .put(customerController.updateCustomerById)
-
 // DELETE "/api/customers/:id"
 .delete(customerController.deleteCustomerById);
-
 
 
 module.exports = router;
