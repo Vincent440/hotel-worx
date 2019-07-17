3704fb0d2087a9305d79705a9880a63c1c47a938
mike4747 <mike4747@oh.rr.com>
2019-07-17 15:45:05 -0400

    committing changes to switch to a new branch

--- config/schema.sql
+++ config/schema.sql
@@ -153,4 +153,19 @@ CREATE TABLE hotel_info (
 
 -- --------------------------------------------------------
 
+CREATE TABLE sessions (
+    session_id varchar(128) COLLATE utf8mb4_bin NOT NULL,
+    expires int(11) unsigned NOT NULL,
+    data mediumtext COLLATE utf8mb4_bin,
+    PRIMARY KEY (session_id)
+);
+
+-- --------------------------------------------------------
+
+CREATE TABLE room_issues (
+    room_issue_id int(10) NOT NULL AUTO_INCREMENT
+);
+
+-- --------------------------------------------------------
+
 set foreign_key_checks=1;
