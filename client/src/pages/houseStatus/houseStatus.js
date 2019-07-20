327ac3f50a61e4f2065f8fa3c73427f1bec9c32d
Sibel <sbaslamisli@yahoo.com>
2019-07-19 23:01:09 -0400

    house status page is done

--- client/src/pages/houseStatus/houseStatus.js
+++ client/src/pages/houseStatus/houseStatus.js
@@ -4,6 +4,10 @@ import { Row, Col } from 'react-grid-system';
 import "./style.css";
 import InfoPart from "../../components/infoPart";
 import Header from "../../components/Header"
+import { Container, Table } from 'react-bootstrap';
+import Particles from "react-particles-js";
+
+const particleOpt = { particles: { number: { value: 120, density: { enable: true, value_area: 1000 } } } };
 
 class HouseStatus extends Component {
     // Setting the initial values of this.state.username and this.state.password
@@ -29,13 +33,11 @@ class HouseStatus extends Component {
         ReservationInfo: {},
         RoomInfo: []
     };
-
     componentDidMount() {
         api.getReservation(1)
             .then(res => this.setState({ ReservationInfo: res.resCust.result[0], RoomInfo: res.resRooms.result }))
             .catch(err => console.log(err))
     }
-
     handleChange = selectedOption => {
         this.setState({ selectedOption });
     }
@@ -60,97 +62,118 @@ class HouseStatus extends Component {
     render() {
         return (
 
-            <Row id="dashboardTable1">
-                <InfoPart user={this.props.user} logout={this.props.logout} />
-                <Col sm={10}>
-                    <row>
-                        <Header>HOUSE STATUS</Header>
-                        <form>
-                            <div id="resHouse">
-                                {this.state.RoomInfo.map((room, i) => (
-                                    <div key={room.res_room_id}>
-                                        <table>
-                                            <tr>
-                                                <th><p>Room Summary</p></th>
-                                                <th><p>Activity</p></th>
-                                                <th><p>Housekeeping Room Status</p></th>
-                                            </tr>
+            <Container>
+                <Particles params={particleOpt} id="particul" />
 
-                                            <td>
-                                                <tr>
-                                                    <td><p> Total Rooms to Sell: {}</p></td>
-                                                </tr>
+                <Row>
+                    <Col xs={12} sm={12} md={2} lg={2} xl={2}>
+                        <InfoPart user={this.props.user} logout={this.props.logout} />
+                    </Col>
+                    <Col xs={12} sm={12} md={10} lg={10} xl={10}>
+                        <Row>
+                            <Col xs={12} sm={12} md={12} lg={12} xl={12}>
+                                <Header>HOUSE STATUS</Header>
+                            </Col>
+                        </Row>
+                        <div id="resHouse">
+                            <Row>
+                                <Col xs={12} sm={12} md={6} lg={4} xl={4}>
+                                    <Row id="headTop">
+                                        Room Summary
+                                                </Row>
+                                    <Row id="rowHouse">
+                                        Total Rooms to Sell: {}
+                                    </Row>
+                                    <Row id="rowHouse">
+                                        Min. Available Tonight:{}
+                                    </Row>
+                                    <Row id="rowHouse">
+                                        Max. Occupied Tonight:{}
+                                    </Row>
+                                </Col>
+                                <Col xs={12} sm={12} md={6} lg={4} xl={4}>
+                                    <Row id="headTop">
+                                        Activity
+                                            </Row>
+                                    <Row id="rowHouse">
+                                        Stayovers: {}
+                                    </Row>
+                                    <Row id="rowHouse">
+                                        Departures Expected: {}
+                                    </Row>
+                                    <Row id="rowHouse">
+                                        Departures Actual: {}
+                                    </Row>
+                                    <Row id="rowHouse">
+                                        Arrivals Expected: {}
+                                    </Row>
+                                    <Row id="rowHouse">
+                                        Arrivals Actual: {}
+                                    </Row>
+                                </Col>
+                                <Col xs={12} sm={12} md={6} lg={4} xl={4}>
+                                    <Row id="headTop">
+                                        Room Status-Housekeeping 
+                                                </Row>
+                                    <Row id="rowHouse2">
+                                        <Col xs={3} sm={3} md={2} lg={4} xl={4}>
+                                        </Col>
+                                        <Col xs={3} sm={3} md={2} lg={4} xl={4}>
+                                            Vacant
+                                                </Col>
+                                        <Col xs={3} sm={3} md={2} lg={4} xl={4}>
+                                            Occupied
+                                                </Col>
+                                    </Row>
+                                    <Row id="rowHouse2">
+                                        <Col xs={3} sm={3} md={2} lg={4} xl={4}>
+                                            Clean
+                                                </Col>
+                                        <Col xs={3} sm={3} md={2} lg={4} xl={4}>
+                                            vacantClean
+                                                    </Col>
+                                        <Col xs={3} sm={3} md={2} lg={4} xl={4}>
+                                            occupiedClean
+                                                </Col>
+                                    </Row>
+                                    <Row id="rowHouse2">
+                                        <Col xs={3} sm={3} md={2} lg={4} xl={4}>
+                                            Dirty
+                                                     </Col>
+                                        <Col xs={3} sm={3} md={2} lg={4} xl={4}>
+                                            vacantDirty
+                                                </Col>
+                                        <Col xs={3} sm={3} md={2} lg={4} xl={4}>
+                                            occupiedDirty
+                                                </Col>
+                                    </Row>
 
-                                                <tr>
-                                                    <td><p>Min. Available Tonight:{}</p></td>
-                                                </tr>
-                                                <tr>
-                                                    <td><p>Max. Occupied Tonight{} </p></td>
-                                                </tr>
-                                            </td>
-                                            <td>
-
-                                                <tr>
-                                                    <td><p>Stayovers: {}</p></td>
-                                                </tr>
-                                                <tr>
-                                                    <td><p>Departures Expected: {}</p></td>
-                                                </tr>
-                                                <tr>
-                                                    <td><p>Departures Actual: {}</p></td>
-                                                </tr>
-                                                <tr>
-                                                    <td><p>Arrivals Expected: {}</p></td>
-                                                </tr>
-                                                <tr>
-                                                    <td><p>Arrivals Actual: {}</p></td>
-                                                </tr>
-                                            </td>
-                                            <td>
-                                                <tr>
-                                                    <tr>
-                                                        <td></td>
-                                                        <td><p>Vacant</p></td>
-                                                        <td><p>Occupied</p></td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>Clean</td>
-                                                        <td id="vacantClean" style={{ border: "1px solid black", width: "40px;" }}>{}</td>
-                                                        <td id="occupiedClean" style={{ border: "1px solid black", width: "30px;" }}>{}</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>Dirty</td>
-                                                        <td id="vacantDirty" style={{ border: "1px solid black", width: "40px;" }}>{}</td>
-                                                        <td id="occupiedDirty" style={{ border: "1px solid black", width: "30px;" }}>{}</td>
-                                                    </tr>
-                                                </tr>
-                                            </td>
-
-                                            <tr style={{ marginTop: "20px;", }} >
-                                                <td style={{ textAlign: "right", marginLeft: "20px;", }}><p>Date</p></td>
-                                                <td><input
-                                                    id=""
-                                                    type="date"
-                                                    name="arrivaldate"
-                                                    value={this.state.arrivaldate}
-                                                    onChange={this.handleInputChange}
-                                                /></td>
-                                            </tr>
-
-                                        </table>
-                                    </div>
-                                ))}
+                                </Col>
+                            </Row>
+                            <div id="dateRow">
+                                <tr >
+                                    <td>Date</td>
+                                    <td>
+                                        <input style={{ width: "150px", height: "30px" }}
+                                            type="date"
+                                            name="arrivaldate"
+                                            value={this.state.arrivaldate}
+                                            onChange={this.handleInputChange}
+                                        />
+                                    </td>
+                                </tr>
                             </div>
+
                             <div id="buttonDiv">
-                                <button type="button" className="btn btn-primary" style={{marginLeft:"450px"}}>Search</button>
+                                <button type="button" className="btn btn-primary" style={{ marginLeft: "450px" }}>Search</button>
 
-                                <button type="button" className="btn btn-primary" style={{marginLeft:"20px"}}>Close</button>
+                                <button type="button" className="btn btn-primary" style={{ marginLeft: "20px" }}>Close</button>
 
                             </div>
-                        </form>
-                    </row>
-                </Col >
-            </Row >
+                        </div>
+                    </Col>
+                </Row>
+            </Container >
         )
     }
 }
