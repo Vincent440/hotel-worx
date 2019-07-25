870b3e3ec922c7bd9b04cbdfb5da92811a93984c
mike4747 <mike4747@oh.rr.com>
2019-07-25 00:35:49 -0400

    added links to the update reservation page on the reservation confirmation page and on the rows in house status, fixed the bug in house status where it was returning no results in activy if there was no activity for that day... now it's showing zeros

--- client/src/pages/newreservation/reservationTest.js
+++ client/src/pages/newreservation/reservationTest.js
@@ -3,12 +3,14 @@ import { Row, Col } from 'react-grid-system';
 import "./style.css";
 import api from '../../utils/api';
 import Header from "../../components/Header";
+import { Redirect } from "react-router-dom";
 
 class ReservationTest extends Component {
     state = {
         ReservationInfo: {},
         RoomInfo: [],
-        reservation_id: ""
+        reservation_id: "",
+        reservationChosen: false
     };
 
     componentDidMount() {
@@ -23,95 +25,107 @@ class ReservationTest extends Component {
         });
     }
 
-    printFunction(){
+    handleChosenReservation = id => {
+        this.setState({ reservationChosen: true });
+    }
+
+    printFunction() {
         window.print();
-      }
+    }
 
     render() {
+        if (this.state.reservationChosen) {
+            localStorage.setItem('reservation_id', this.state.reservation_id);
+            return (
+                <Redirect to={{
+                    pathname: '/reserve/testUpdatereservation'
+                }} />
+            )
+        }
+
         return (
-        <div>
-            <Row>
-                <Col xl={12}>
-                    <Header>RESERVATION CONFIRMATION</Header>
-                </Col>
-            </Row>
-            <Row>
-                <Col xl={12}>
-                    <div id="res3" style={{ paddingBottom: "10px" }}>
-                        <Row style={{ paddingBottom: "5px" }}>
-                            <Col xl={3}>
-                            </Col>
-                            <Col xl={5}>
-                            <strong>Name: </strong>{this.state.ReservationInfo.first_name} {this.state.ReservationInfo.last_name}
-                            </Col>
-                        </Row>
-                        <Row style={{ paddingBottom: "5px" }}>
-                            <Col xl={3}>
-                            </Col>
-                            <Col xl={9}>
-                                <strong>Address: </strong>{this.state.ReservationInfo.address},  {this.state.ReservationInfo.city}, {this.state.ReservationInfo.state}, {this.state.ReservationInfo.zip}
-                            </Col>
-                        </Row>
-                        <Row style={{ paddingBottom: "5px" }}>
-                            <Col xl={3}>
-                            </Col>
-                            <Col xl={3}>
-                                <strong>Email: </strong> {this.state.ReservationInfo.email}
-                            </Col>
-                            <Col xl={4}>
-                                <strong>Phone: </strong> {this.state.ReservationInfo.phone}
-                            </Col>
-                        </Row>
-                        <Row style={{ paddingBottom: "5px" }}>
-                            <Col xl={3}>
-                            </Col>
-                            <Col xl={3}>
-                                <strong>Credit Card Number: </strong>****{this.state.ReservationInfo.ccLastFour}
-                            </Col>
-                            <Col xl={3}>
-                                <strong>Exp Date: </strong>{this.state.ReservationInfo.cc_expiration}
-                            </Col>
-                        </Row>
-                        <hr />
-                        <Row style={{ paddingBottom: "5px" }}>
-                            <Col xl={3} >
-                            </Col>
-                            {this.state.RoomInfo.map((room) => (
-                                <div key={room.res_room_id}>
-                                    <ul style={{ marginLeft: "-25px" }}>
-                                        <li><strong>Confirmation Number: </strong>{room.confirmation_code}</li>
-                                        <li><strong>Arrival Date: </strong>{room.check_in_date}</li>
-                                        <li><strong>Departure Date: </strong>{room.check_out_date}</li>
-                                        <li><strong>Adults: </strong>{room.adults}</li>
-                                        <li><strong>Rate: </strong>{room.rate}</li>
-                                        <li><strong>Room Type: </strong>{room.type}</li>
-                                        <li><strong>Comments: </strong>{room.comments}</li>
-                                    </ul>
-                                </div>
-                            ))}
-                        </Row>
+            <div>
+                <Row>
+                    <Col xl={12}>
+                        <Header>RESERVATION CONFIRMATION</Header>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xl={12}>
+                        <div id="res3" style={{ paddingBottom: "10px" }}>
+                            <Row style={{ paddingBottom: "5px" }}>
+                                <Col xl={3}>
+                                </Col>
+                                <Col xl={5}>
+                                    <strong>Name: </strong>{this.state.ReservationInfo.first_name} {this.state.ReservationInfo.last_name}
+                                </Col>
+                            </Row>
+                            <Row style={{ paddingBottom: "5px" }}>
+                                <Col xl={3}>
+                                </Col>
+                                <Col xl={9}>
+                                    <strong>Address: </strong>{this.state.ReservationInfo.address},  {this.state.ReservationInfo.city}, {this.state.ReservationInfo.state}, {this.state.ReservationInfo.zip}
+                                </Col>
+                            </Row>
+                            <Row style={{ paddingBottom: "5px" }}>
+                                <Col xl={3}>
+                                </Col>
+                                <Col xl={3}>
+                                    <strong>Email: </strong> {this.state.ReservationInfo.email}
+                                </Col>
+                                <Col xl={4}>
+                                    <strong>Phone: </strong> {this.state.ReservationInfo.phone}
+                                </Col>
+                            </Row>
+                            <Row style={{ paddingBottom: "5px" }}>
+                                <Col xl={3}>
+                                </Col>
+                                <Col xl={3}>
+                                    <strong>Credit Card Number: </strong>****{this.state.ReservationInfo.ccLastFour}
+                                </Col>
+                                <Col xl={3}>
+                                    <strong>Exp Date: </strong>{this.state.ReservationInfo.cc_expiration}
+                                </Col>
+                            </Row>
+                            <hr />
+                            <Row style={{ paddingBottom: "5px" }}>
+                                <Col xl={3} >
+                                </Col>
+                                {this.state.RoomInfo.map((room) => (
+                                    <div key={room.res_room_id}>
+                                        <ul style={{ marginLeft: "-25px" }}>
+                                            <li><strong>Confirmation Number: </strong>{room.confirmation_code}</li>
+                                            <li><strong>Arrival Date: </strong>{room.check_in_date}</li>
+                                            <li><strong>Departure Date: </strong>{room.check_out_date}</li>
+                                            <li><strong>Adults: </strong>{room.adults}</li>
+                                            <li><strong>Rate: </strong>{room.rate}</li>
+                                            <li><strong>Room Type: </strong>{room.type}</li>
+                                            <li><strong>Comments: </strong>{room.comments}</li>
+                                        </ul>
+                                    </div>
+                                ))}
+                            </Row>
 
-                        <Row>
-                            <Col xl={3}>
-                            </Col>
-                            <Col xl={2}>
-                                Made by User ID: {this.state.ReservationInfo.user_id}
-                            </Col>
-                            <Col xl={3}>
-                                Created On: {this.state.ReservationInfo.created_at}
-                            </Col>
-                        </Row>
-                    </div>
-                </Col>
-            </Row>
-            <div id="buttonsReservation">
-                <button type="submit" className="btn btn-primary" style={{ marginLeft: "480px" }} onClick={this.props.handleFormSubmit}>Change</button>
-                <button type="submit" className="btn btn-primary" style={{ marginLeft: "10px" }} onClick={this.printFunction}>Print</button>
-                <button type="submit" className="btn btn-primary" style={{ marginLeft: "10px" }} value="Send"><a href={"mailto:" + this.state.ReservationInfo.email}>Email</a></button>
-                <button type="submit" className="btn btn-primary" style={{ marginLeft: "10px" }} onClick={this.props.handleFormSubmit}>Save</button>
-
+                            <Row>
+                                <Col xl={3}>
+                                </Col>
+                                <Col xl={2}>
+                                    Made by User ID: {this.state.ReservationInfo.user_id}
+                                </Col>
+                                <Col xl={3}>
+                                    Created On: {this.state.ReservationInfo.created_at}
+                                </Col>
+                            </Row>
+                        </div>
+                    </Col>
+                </Row>
+                <div id="buttonsReservation">
+                    <button type="submit" className="btn btn-primary" style={{ marginLeft: "480px" }} onClick={() => this.handleChosenReservation(this.state.reservation_id)}>Change</button>
+                    <button type="submit" className="btn btn-primary" style={{ marginLeft: "10px" }} onClick={this.printFunction}>Print</button>
+                    <button type="submit" className="btn btn-primary" style={{ marginLeft: "10px" }} value="Send"><a href={"mailto:" + this.state.ReservationInfo.email}>Email</a></button>
+                    {/* <button type="submit" className="btn btn-primary" style={{ marginLeft: "10px" }} onClick={this.props.handleFormSubmit}>Save</button> */}
+                </div>
             </div>
-        </div>
         )
     }
 }
--- models/res_room.js
+++ models/res_room.js
@@ -3,7 +3,7 @@ const connection = require("../config/connection");
 const ResRoom = {
     selectForHouseStatus: (date, cb) => {
         const preQueryString = "SET @input_date=?;";
-        const queryString = "SELECT SUM(CASE WHEN rr.check_in_date<@input_date && rr.check_out_date>@input_date THEN 1 ELSE 0 END) AS stayovers, SUM(CASE WHEN rr.check_out_date=@input_date && rr.checked_out=0 THEN 1 ELSE 0 END) AS departuresPending, SUM(CASE WHEN rr.check_out_date=@input_date && rr.checked_out=1 THEN 1 ELSE 0 END) AS departuresActual, SUM(CASE WHEN rr.check_in_date=@input_date && rr.checked_in=0 THEN 1 ELSE 0 END) AS arrivalsPending, SUM(CASE WHEN rr.check_in_date=@input_date && rr.checked_in=1 THEN 1 ELSE 0 END) AS arrivalsActual FROM res_rooms AS rr WHERE rr.active=1 && (rr.check_in_date<@input_date && rr.check_out_date>@input_date) || rr.check_out_date=@input_date || rr.check_in_date=@input_date;";
+        const queryString = "SELECT COALESCE(SUM(CASE WHEN rr.check_in_date<@input_date && rr.check_out_date>@input_date THEN 1 ELSE 0 END), 0) AS stayovers, COALESCE(SUM(CASE WHEN rr.check_out_date=@input_date && rr.checked_out=0 THEN 1 ELSE 0 END), 0) AS departuresPending, COALESCE(SUM(CASE WHEN rr.check_out_date=@input_date && rr.checked_out=1 THEN 1 ELSE 0 END), 0) AS departuresActual, COALESCE(SUM(CASE WHEN rr.check_in_date=@input_date && rr.checked_in=0 THEN 1 ELSE 0 END), 0) AS arrivalsPending, COALESCE(SUM(CASE WHEN rr.check_in_date=@input_date && rr.checked_in=1 THEN 1 ELSE 0 END), 0) AS arrivalsActual FROM res_rooms AS rr WHERE rr.active=1 && (rr.check_in_date<@input_date && rr.check_out_date>@input_date) || rr.check_out_date=@input_date || rr.check_in_date=@input_date;";
         connection.query(preQueryString + queryString, [date], (err, result) => {
             if (err) throw err;
             cb(result[1]);
@@ -48,7 +48,7 @@ const ResRoom = {
     },
     getGuests: (conditions, cb) => {
         formattedConditions = conditions.join(" && ");
-        const queryString = "SELECT DATE_FORMAT(rr.check_in_date, '%b %d, %Y') AS check_in_date, DATE_FORMAT(rr.check_out_date, '%b %d, %Y') AS check_out_date, rm.room_num, rr.confirmation_code, rr.comments, rt.type, c.first_name, c.last_name FROM res_rooms AS rr INNER JOIN room_types AS rt ON rr.room_type_id=rt.room_type_id INNER JOIN rooms AS rm ON rm.room_id=rr.room_id INNER JOIN reservations AS r ON rr.reservation_id=r.reservation_id INNER JOIN customers AS c ON c.customer_id=r.customer_id WHERE rr.active=1 && rr.checked_in=1 && rr.checked_out=0 && " + formattedConditions + " ORDER BY rm.room_num ASC;";
+        const queryString = "SELECT r.reservation_id, DATE_FORMAT(rr.check_in_date, '%b %d, %Y') AS check_in_date, DATE_FORMAT(rr.check_out_date, '%b %d, %Y') AS check_out_date, rm.room_num, rr.confirmation_code, rr.comments, rt.type, c.first_name, c.last_name FROM res_rooms AS rr INNER JOIN room_types AS rt ON rr.room_type_id=rt.room_type_id INNER JOIN rooms AS rm ON rm.room_id=rr.room_id INNER JOIN reservations AS r ON rr.reservation_id=r.reservation_id INNER JOIN customers AS c ON c.customer_id=r.customer_id WHERE rr.active=1 && rr.checked_in=1 && rr.checked_out=0 && " + formattedConditions + " ORDER BY rm.room_num ASC;";
         connection.query(queryString, (err, result) => {
             if (err) throw err;
             cb(result);
