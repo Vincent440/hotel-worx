7bf87e4b1975b75a397837f33be64a8ce0991577
mike4747 <mike4747@oh.rr.com>
2019-07-25 09:14:14 -0400

    added user_id to update reseravtion so the last user that updated the reseravtion is now the user associated with that reservation

--- client/src/pages/updatereservation/updatereservation.js
+++ client/src/pages/updatereservation/updatereservation.js
@@ -183,7 +183,8 @@ class ReserveUpdate extends Component {
             roomtype: this.state.roomtype,
             resRoomId: this.state.resRoomId,
             comments: this.state.comments,
-            rate: this.state.rate
+            rate: this.state.rate,
+            user_id: this.props.user.user_id
         }
         api.updateReservation(data)
             .then(() => this.setState({ updateSuccess: true, cancelSuccess: false, active: 1 }))
