0fc0c78500ecde9f3f784c1e266dc072c7dd71f9
Vincent440 <vinceshury@gmail.com>
2021-04-14 13:20:20 -0400

    Disabling unused routes with comments

--- routes/api/room-types.js
+++ routes/api/room-types.js
@@ -1,27 +1,30 @@
 const router = require('express').Router()
 const roomTypeController = require('../../controllers/roomTypeController')
+
 /*
+  INACTIVE - POST
   POST "api/room/types" Example Request: { "vals": ["2Double", 109.99] }
   GET "api/room/types"
 */
 
-// INACTIVE - POST
 router
   .route('/')
-  .post(roomTypeController.createNewRoomType)
   .get(roomTypeController.getAllRoomTypes)
+  // .post(roomTypeController.createNewRoomType)
+
 /*
+  INACTIVE - ALL
   GET "api/room/types/:id"
   PUT "api/room/types/:id" Example Request: { "vals": ["2Double", 109.99] }
   DELETE "api/room/types/:id"
 */
 
-// INACTIVE - ALL
-router
-  .route('/:id')
-  .get(roomTypeController.getRoomTypeById)
-  .put(roomTypeController.updateRoomTypeById)
-  .delete(roomTypeController.deleteRoomTypeById)
+ 
+// router
+//   .route('/:id')
+//   .get(roomTypeController.getRoomTypeById)
+//   .put(roomTypeController.updateRoomTypeById)
+//   .delete(roomTypeController.deleteRoomTypeById)
 
 /*
   GET "api/room/types/available/:date"
